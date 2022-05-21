c8193b150069925eee3d5576208c23499ab6502e
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-05-21 18:51:36 +0000

    started js code

--- js/script.js
+++ js/script.js
@@ -12,24 +12,24 @@
 function displayProduct () {
 	
 	// get user input for min
-	let min = parseInt(document.getElementById('min').value)
+	let userInt1 = parseInt(document.getElementById('userInt1').value)
 
 	// get user input for max
-	let max = parseInt(document.getElementById('max').value)
+	let userInt2 = parseInt(document.getElementById('userInt2').value)
 
  // initialize counter
-	let counter = min
+	let counter = userInt1
 
   // initialize numbers as empty string
-  let numbers = ""
+  let product = ""
   
 	// WHILE loop
-	while (counter <= max) {
+	for (counter <= userInt2) {
     // make number string with comma's
-    numbers = numbers + counter + ", "
+    product = product + userInt1 + ", "
     // increment the counter
     counter++
   }
   // display sequence back to user
-	document.getElementById("message").innerHTML = numbers
+	document.getElementById("message").innerHTML = product
 } 
