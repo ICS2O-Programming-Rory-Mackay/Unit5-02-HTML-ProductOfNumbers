38de8a38a5b0d25185bd01c761f46ddee699df76
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-05-27 13:40:10 +0000

    fixed js

--- js/script.js
+++ js/script.js
@@ -10,23 +10,20 @@
  * This function displays the product of the users two positive integers using addition
  */
 function displayProduct () {
-	
+
+   // initialize numbers as empty string
+  let product = 0
+  
 	// get user input for min
 	let userInt1 = parseInt(document.getElementById('userInt1').value)
 
 	// get user input for max
 	let userInt2 = parseInt(document.getElementById('userInt2').value)
-
- // initialize counter
-	let counter = userInt1
-
-  // initialize numbers as empty string
-  let product = ""
   
 	// WHILE loop
-	for (let counter = 0; counter <= userInt2; counter++) {
+	for (let counter = 1; counter <= userInt2; counter++) {
     // make number string with comma's
-    product = product + userInt1 + ", "
+    product = product + userInt1
   }
   // display sequence back to user
 	document.getElementById("message").innerHTML = product
